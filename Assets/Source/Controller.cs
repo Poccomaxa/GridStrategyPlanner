e7d7ff794e5bb0559c4a563fcc92218521cd03f7
a_lebedev <poc.leb@mail.ru>
2021-07-21 00:24:16 +0300

    Zoom up down

--- Assets/Source/Controller.cs
+++ Assets/Source/Controller.cs
@@ -9,7 +9,12 @@ public class Controller : MonoBehaviour
     Camera MainCamera;
     Vector2 CameraSpeed;
     float RotationDirectionSpeed;
+
     public float CameraSensitivy;
+    public float CameraRotationSensitivity;
+    public float MinZoom;
+    public float MaxZoom;
+    public int Steps;
 
     private void Awake()
     {
@@ -20,12 +25,42 @@ public class Controller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        UpdateZoom(0);
     }
 
     private void OnDestroy()
     {
 
+    }
+
+    bool CameraPlaneIntersection(out Vector3 Intersection)
+    {
+        Plane Floor = new Plane(Vector3.up, Vector3.zero);
+        float Enter = 0;
+        Ray LookingForFloor = new Ray(MainCamera.transform.position, MainCamera.transform.forward);
+        if (Floor.Raycast(LookingForFloor, out Enter))
+        {
+            Intersection = LookingForFloor.origin + LookingForFloor.direction * Enter;
+            return true;
+        }
+        
+        Intersection = Vector3.zero;
+        return false;        
+    }
+
+    void UpdateZoom(int Scroll)
+    {
+        if (CameraPlaneIntersection(out Vector3 PlaneIntersection))
+        {
+            Vector3 CameraArm = MainCamera.transform.position - PlaneIntersection;
+            float CurrentDistance = Mathf.Max(0, CameraArm.magnitude - MinZoom);
+            float StepDistance = (MaxZoom - MinZoom) / Steps;
+            int CurrentSteps = Mathf.RoundToInt(CurrentDistance / StepDistance);
+            CurrentSteps = Mathf.Clamp(CurrentSteps + (int)Scroll, 0, Steps);
+
+            float NewDistance = CurrentSteps * StepDistance + MinZoom;
+            MainCamera.transform.position = PlaneIntersection + CameraArm.normalized * NewDistance;
+        }
     }
 
     // Update is called once per frame
@@ -37,12 +72,8 @@ public class Controller : MonoBehaviour
         Vector2 CameraSpeedRotated = Quaternion.Euler(0, 0, Angle) * CameraSpeed;
         MainCamera.transform.position += new Vector3(CameraSpeedRotated.x, 0, CameraSpeedRotated.y);
 
-        Plane Floor = new Plane(Vector3.up, Vector3.zero);
-        float Enter = 0;
-        Ray LookingForFloor = new Ray(MainCamera.transform.position, MainCamera.transform.forward);
-        if (Floor.Raycast(LookingForFloor, out Enter))
+        if (CameraPlaneIntersection(out Vector3 RotateAround))
         {
-            Vector3 RotateAround = LookingForFloor.origin + LookingForFloor.direction * Enter;
             MainCamera.transform.RotateAround(RotateAround, Vector3.up, RotationDirectionSpeed);
         }        
     }
@@ -54,6 +85,17 @@ public class Controller : MonoBehaviour
 
     public void OnRotate(InputAction.CallbackContext callbackContext)
     {
-        RotationDirectionSpeed = callbackContext.ReadValue<System.Single>();        
+        RotationDirectionSpeed = callbackContext.ReadValue<System.Single>() * CameraRotationSensitivity;        
+    }
+
+    public void OnZoom(InputAction.CallbackContext callbackContext)
+    {
+
+        float ScrollValue = callbackContext.ReadValue<Vector2>().y;
+        if (Mathf.Abs(ScrollValue) > float.Epsilon)
+        {
+            int Scroll = (int)Mathf.Sign(ScrollValue) * -1;
+            UpdateZoom(Scroll);            
+        }
     }
 }
