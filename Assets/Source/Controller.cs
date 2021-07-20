738df39244fcf0b586728339316e1c3feb8ab00f
a_lebedev <poc.leb@mail.ru>
2021-07-20 23:42:25 +0300

    Camera rotation and movement

--- Assets/Source/Controller.cs
+++ Assets/Source/Controller.cs
@@ -8,6 +8,7 @@ public class Controller : MonoBehaviour
     PlayerInput PlayerInput;
     Camera MainCamera;
     Vector2 CameraSpeed;
+    float RotationDirectionSpeed;
     public float CameraSensitivy;
 
     private void Awake()
@@ -29,17 +30,30 @@ public class Controller : MonoBehaviour
 
     // Update is called once per frame
     void FixedUpdate()
-    {
-        Vector3 Orientation = MainCamera.transform.forward;
-        MainCamera.transform.position += new Vector3(CameraSpeed.x, 0, CameraSpeed.y);
+    {        
+        Vector2 Orientation = new Vector2(MainCamera.transform.forward.x, MainCamera.transform.forward.z);
+        Orientation.Normalize();
+        float Angle = Vector2.SignedAngle(Vector2.up, Orientation);        
+        Vector2 CameraSpeedRotated = Quaternion.Euler(0, 0, Angle) * CameraSpeed;
+        MainCamera.transform.position += new Vector3(CameraSpeedRotated.x, 0, CameraSpeedRotated.y);
+
+        Plane Floor = new Plane(Vector3.up, Vector3.zero);
+        float Enter = 0;
+        Ray LookingForFloor = new Ray(MainCamera.transform.position, MainCamera.transform.forward);
+        if (Floor.Raycast(LookingForFloor, out Enter))
+        {
+            Vector3 RotateAround = LookingForFloor.origin + LookingForFloor.direction * Enter;
+            MainCamera.transform.RotateAround(RotateAround, Vector3.up, RotationDirectionSpeed);
+        }        
     }
 
     public void OnNavigate(InputAction.CallbackContext callbackContext)
     {        
-        CameraSpeed = callbackContext.ReadValue<Vector2>() * CameraSensitivy;        
-        if (callbackContext.canceled)
-        {
-            CameraSpeed = Vector2.zero;
-        }        
+        CameraSpeed = callbackContext.ReadValue<Vector2>() * CameraSensitivy;     
+    }
+
+    public void OnRotate(InputAction.CallbackContext callbackContext)
+    {
+        RotationDirectionSpeed = callbackContext.ReadValue<System.Single>();        
     }
 }
